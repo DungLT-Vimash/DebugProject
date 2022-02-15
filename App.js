87e2f26fee65a5c152287f61adb8438c186de12d
Lê Tiến Dũng <33333812+letiendungdn@users.noreply.github.com>
2022-02-15 16:20:54 +0700

    first commit

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
+import { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
+class App extends Component{
+  render() {
+    return (
+      <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+    );
+  };
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
